455cead3ef36f1a5fa2aa8fdb18fbd410b32e285
Aswin Aryal <rpalabs@Aswins-MacBook-Pro.local>
2022-02-01 02:06:57 +0545

    update dockerfile and docker compose file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('nodejs')
+import express from 'express';
+
+const app  = express();
+
+app.get('/', (req, res) => {
+    res.send('hello node')
+})
+const port = process.env.PORT || 3000;
+
+app.listen(port)
